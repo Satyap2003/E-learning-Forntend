8c9adbbefc155caa478c6400a0601dc73cf1143b
Priyanshi80935 <165469120+Priyanshi80935@users.noreply.github.com>
2024-10-23 23:57:13 +0530

    frontend changes

--- e-learning-frontend/src/app/app.component.ts
+++ e-learning-frontend/src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'e-learning-frontend';
+
+  constructor(private http : HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:8080/api/course").subscribe((res) => {
+      console.log(res)
+    });
+    
+  }
 }
--- e-learning-frontend/src/app/student/course/course.component.ts
+++ e-learning-frontend/src/app/student/course/course.component.ts
@@ -1,53 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Course } from '../../model/course';
 import { CommonModule } from '@angular/common';
 import { ElipsePipe } from '../../pipe/elipse.pipe';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [CommonModule, ElipsePipe],
+  imports: [CommonModule, ElipsePipe, HttpClientModule],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
-export class CourseComponent {
+export class CourseComponent implements OnInit {
+  constructor(private httpClient : HttpClient) {}
 
-  courses : Course[]= [
-    {
-      id: 1,
-      name: 'Angular',
-      description: 'Angular is a platform and framework for building single-page client applications using HTML and TypeScript.',
-      price: 100,
-      rating: 4,
-      img: 'images/angular.png',
-      duration: '2 months',
-      noOfStudents: 1000,
-      lecture: '12',
-      ratingCount: 103
-    },
-    {
-      id: 2,
-      name: 'React',
-      description: 'React is a declarative, efficient, and flexible JavaScript library for building user interfaces.',
-      price: 200,
-      rating: 3,
-      img: 'images/react.png',
-      duration: '3 months',
-      noOfStudents: 2000,
-      lecture: '15',
-      ratingCount: 100
-    },
-    {
-      id: 3,
-      name: 'Vue',
-      description: 'Vue.js is a progressive framework for building user interfaces.',
-      price: 150,
-      rating: 5,
-      img: 'images/vue.png',
-      duration: '2.5 months',
-      noOfStudents: 1500,
-      lecture: '14',
-      ratingCount: 100
-    }
-  ];
+  courses : Course[] = [];
+
+  ngOnInit(): void {
+      this.httpClient.get("http://localhost:8080/api/course").subscribe((res : any) => {
+          this.courses = res;
+      });
+  }
+  
 }
